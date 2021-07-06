73dc32f7e6c42ff88a61c1a540d60fb5fbdbd8cc
rodol <rodolfo_eliezer@hotmail.com>
2021-07-05 21:49:36 -0300

    Updates

---
+++ precache-manifest.2d501d871cf4c3b8dfcb6ff5b8323e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1008eae1cc870699a2dfa0a493bbc1",
+    "url": "/quadria/index.html"
+  },
+  {
+    "revision": "5b1934d806df5a83fd93",
+    "url": "/quadria/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/quadria/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quadria/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1934d806df5a83fd93",
+    "url": "/quadria/static/js/main.4d64e41c.chunk.js"
+  },
+  {
+    "revision": "846e23967ec4f10f2a3e",
+    "url": "/quadria/static/js/runtime-main.28d12ace.js"
+  }
+]);
